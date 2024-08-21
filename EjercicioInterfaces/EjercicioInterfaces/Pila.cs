1473d399ad1f189b039ecd114e9952e899f5f8f0
DBaigorria320 <412085@tecnicatura.frc.utn.edu.ar>
2024-08-21 18:36:54 -0300

    Ejercio creacion de pila y cola

---
+++ EjercicioInterfaces/EjercicioInterfaces/Pila.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioInterfaces
+{
+    public class Pila : ICollection
+    {
+        public List<object> Objetos { get; set; }
+        public int Contador {  get; set; }
+
+        public Pila() 
+        { 
+            Objetos = new List<object>();
+            Contador = 0;
+        }
+
+        public Pila(List<object> objetos, int contador)
+        {
+            Objetos = objetos;
+            Contador = contador;
+        }
+
+        public bool EstaVacia()
+        {
+            return Objetos.Count == 0;
+        }
+
+        public object Extraer()
+        {
+            if (EstaVacia())
+            {
+                throw new InvalidOperationException("La pila esta vacía!");
+            }
+            else
+            {
+                object obj = Objetos[Objetos.Count - 1];
+                Objetos.RemoveAt(Objetos.Count - 1);
+                Contador--;
+                return obj;
+            }
+        }
+
+        public object Primero()
+        {
+            if(EstaVacia())
+            {
+                throw new InvalidOperationException("La pila esta vacía!");
+            }
+            else
+            {
+                return Objetos[Objetos.Count - 1];
+            }
+        }
+
+        public bool Anadir(object obj)
+        {
+            try
+            {
+                Objetos.Add(obj);
+                Contador++;
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ EjercicioInterfaces/EjercicioInterfaces/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using EjercicioInterfaces;
+
+Pila pila = new Pila();
+
+pila.Anadir(10);
+pila.Anadir(20);
+pila.Anadir(30);
+
+Console.WriteLine($"Primero: {pila.Primero()}"); // Debe imprimir 30
+Console.WriteLine($"Extraer: {pila.Extraer()}"); // Debe imprimir 30
+Console.WriteLine($"Primero: {pila.Primero()}"); // Debe imprimir 20
+Console.WriteLine($"Está vacía: {pila.EstaVacia()}"); // Debe imprimir False
